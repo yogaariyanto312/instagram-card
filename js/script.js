ef6a39bce11093185b6958a6c1417d6f931786ac
yogaariyanto312 <yogaariyanto312@gmail.com>
2025-07-03 02:23:46 +0700

    ngubah total

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+window.addEventListener("load", function () {
+   setTimeout(() => {
+      const loading = document.getElementById("loading");
+      loading.style.opacity = "0";
+      loading.style.transform = "translateY(-20px)";
+      setTimeout(() => {
+         loading.style.display = "none";
+         const container = document.querySelector(".container");
+         container.style.opacity = "1";
+         container.style.transform = "translateY(0)";
+         container.style.boxShadow = "0 0 60px rgba(0, 255, 255, 0.3)";
+      }, 500);
+   }, 2000);
+});
+  
+
+// Glitch Effect btn
+
+document.querySelectorAll(".btn").forEach((btn) => {
+   btn.addEventListener("mouseenter", () => {
+      const rotate = (Math.random() * 4 - 2).toFixed(1); // -2° to +2°
+      const skew = (Math.random() * 4 - 2).toFixed(1); // -2° to +2°
+      btn.style.transform = `scale(1.05) rotate(${rotate}deg) skew(${skew}deg)`;
+   });
+
+   btn.addEventListener("mouseleave", () => {
+      btn.style.transform = "";
+   });
+});
+  
